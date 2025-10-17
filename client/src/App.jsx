1067ebecde30dfc6a7dccdc57969b85dc4d47111
Bishal <bishalbhar709@gmail.com>
2025-10-17 19:26:13 +0530

    implement user authentication with Redux and socket connection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,68 @@
 
 import './App.css'
 import "react-toastify/ReactToastify.css";
-import { Loader } from 'lucide-react';
+import {ToastContainer} from "react-toastify"
+import { Home, Loader } from 'lucide-react';
+import {useDispatch, useSelector} from "react-redux"
+import { useEffect } from 'react';
+import {getUser, setOnlineUser} from "./store/slices/authSlice.js"
+import { connectSocker, disconnectSocket } from './lab/socket.js';
+import {BrowserRouter as Router , Routes,Route, Navigate} from "react-router-dom";
+import {NavBar} from "./components/skeletons/NavBar.jsx";
+import {Home} from "./pages/Home.jsx";
+import {Register} from "./pages/Register.jsx";
+import {Login} from "./pages/Login.jsx";
+import Profile, {Loader} from "./pages/Profile.jsx";
+
+
+
+
+
+
 
 function App() {
 
+  const {authUser,isCheckingAuth} =useSelector((state)=>state.auth);
+
+  const dispatch=useDispatch()
+
+  useEffect(()=>{
+    dispatch(getUser());
+  },[getUser]);
+
+  useEffect(()=>{
+    if(authUser){
+      const socket=connectSocker(authUser._id);
+      socket.on("getOnlineUsers",(users)=>{
+        dispatch(setOnlineUser())
+      });
+
+      return ()=>disconnectSocket();
+    }
+  },[authUser])
+
+  if(isCheckingAuth && !authUser){
+return(
+  <div className='flex items-center justify-center h-screen'>
+    <Loader className='size-10 animate-spin'/>
+  </div>
+)
+  }
+
   return (
     <>
-    <h1 class="text-3xl font-bold underline">Hello world! back again in the  business
-        </h1>
+
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route  path='/' element={authUser ? <Home/>: <Navigate to={"/login"} />}/>
+        <Route  path='/register' element={authUser ? <Register/>: <Navigate to={"/"} />}/>
+        <Route  path='/login' element={authUser ? <Login/>: <Navigate to={"/"} />}/>
+        <Route  path='/profile' element={authUser ? <Profile/>: <Navigate to={"/login"} />}/>
+      </Routes>
+      <ToastContainer />
+    </Router>
+    
     </>
   )
 }
