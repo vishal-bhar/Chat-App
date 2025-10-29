80d3896d5c22e13577844b63eb0317f89d7b5d27
Bishal <bishalbhar709@gmail.com>
2025-10-29 16:49:43 +0530

    maded some component ,skeletons

--- client/src/components/NoChatSelected.jsx
+++ client/src/components/NoChatSelected.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+
+
+import {MessageSquare} from "lucide-react" 
 
 function NoChatSelected() {
+
+
+
   return (
-    <div>NoChatSelected</div>
-  )
+  <>
+  <div className="w-full flex flex-col items-center justify-center p-16 bg-white/50">
+  <div className="max-w-md text-center space-y-6">
+    {/* Icon Display */}
+    <div className="flex justify-center gap-4 mb-4">
+      <div className="relative">
+        <div className="w-16 h-16 rounded-2xl bg-blue-100 flex items-center justify-center animate-bounce">
+          <MessageSquare />
+        </div>
+      </div>
+    </div>
+
+    {/* Welcome text */}
+
+    <h2 className="text-2xl font-bold text-gray-800">
+      Welcome to Talkie
+    </h2>
+    <p className="text-gray-500">Select a conversation from sidebar to start chatting</p>
+
+  </div>
+  </div>
+  </>)
 }
 
 export default NoChatSelected
--- client/src/components/skeletons/SidebarSkeleton.jsx
+++ client/src/components/skeletons/SidebarSkeleton.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import { User } from "lucide-react";
 
 function SidebarSkeleton() {
+
+
+  const skeletonContacts=Array(8).fill(null);
+
   return (
-    <div>SidebarSkeleton some thing went wrong</div>
+    <>
+    <aside className='h-full w-20 lg:w-72 border-r border-gray-200 flex flex-col transition-all duration-200'>
+      {/* Header */}
+      <div className="border-b border-gray-200 2-full p-5">
+          <div className="flex items-center gap-2">
+            <User className="w-6 h-6 text-gray-700" />
+            <span className="font-medium hidden lg:block text-gray-800">
+              Contact
+            </span>
+          </div>
+          </div>
+          
+
+    {/* Skeletion contact */}
+
+    <div className='overflow-y-auto w-full py-3'>
+      {
+        skeletonContacts.map((_,index)=>{
+          return (
+            <div key={index} className='w-full p-3 flex items-center gap-3 animate-pulse'>
+              {/* Avatar Skeleton */}
+              <div className='relative mx-auto lg:mx-0'>
+                <div className='w-12 h-12 bg-gray-300 rounded-full' /> 
+              </div>
+
+              {/* Text skeleton for large screen only */}
+
+              <div className='hidden lg:flex flex-col gap-2 flex-1'>
+                <div className='h-4 w-32 bg-gray-300 rounded' />
+                <div className='h-4 w-32 bg-gray-300 rounded' />
+                <div className='h-4 w-32 bg-gray-300 rounded' />
+              </div>
+            </div>
+          );
+        })
+      }
+    </div>
+
+    </aside>
+    </>
   )
 }
 
